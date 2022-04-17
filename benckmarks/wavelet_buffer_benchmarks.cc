18508c302e288a31819c68ed50158a385b9fa2ae
Kataev Victor <kataev.victor.1234@gmail.com>
2022-04-17 11:28:16 +0000

    init benchmarks

---
+++ benckmarks/wavelet_buffer_benchmarks.cc
@@ -0,0 +1,228 @@
+// Copyright 2020-2022 PANDA GmbH
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
+#include <catch2/catch.hpp>
+#include <drift/dsp/primitives.h>
+#include <drift/dsp/denoise_algorithms.h>
+#include <drift/dsp/wavelet_buffer.h>
+#include <drift/dsp/wavelet_parameters.h>
+#include <drift/dsp/wavelet_utils.h>
+#include <drift/utils/init.h>
+#include <metric/modules/transform/wavelet.hpp>
+
+#include <fstream>
+
+using drift::dsp::DataType;
+using drift::dsp::Signal1D;
+using drift::dsp::SignalN2D;
+using drift::dsp::WaveletBuffer;
+using drift::utils::GetRandomSignal;
+
+TEST_CASE("Wavelet algorithms benchmark 1D") {
+  using drift::dsp::NullDenoiseAlgorithm;
+
+  auto k = GENERATE(0.1, 1, 60);
+
+  const auto length = static_cast<size_t>(k * 48000);
+
+  drift::dsp::WaveletParameters parameters = {
+      .signal_shape = {length},
+      .signal_number = 1,
+      .decomposition_steps = 9,
+      .wavelet_type = drift::dsp::WaveletTypes::kDB3,
+      .is_raw_convolve_1d = true};
+
+  auto data_src = GetRandomSignal(length);
+
+  WaveletBuffer buffer(parameters);
+  BENCHMARK("Decompose " + std::to_string(length)) {
+    buffer.Decompose(data_src, NullDenoiseAlgorithm<DataType>());
+  };
+
+  BENCHMARK("Compose " + std::to_string(length)) {
+    Signal1D data_dst;
+    buffer.Compose(&data_dst);
+  };
+}
+
+TEST_CASE("Convolution of long 1D signal") {
+  auto k = GENERATE(0.1, 1, 60);
+  const size_t length = k * 48000;
+  const auto signal = GetRandomSignal(length);
+
+  const auto [lo_d, hi_d, lo_r, hi_r] = wavelet::orthfilt(
+      wavelet::dbwavf<blaze::DynamicVector<DataType>>(3, DataType()));
+  blaze::CompressedMatrix<DataType> dmat(2, lo_d.size());
+  blaze::row(dmat, 0) = blaze::trans(blaze::reverse(lo_d));
+  blaze::row(dmat, 1) = blaze::trans(blaze::reverse(hi_d));
+  BENCHMARK("Raw convolve") {
+    auto result = drift::dsp::internal::dwt(signal, dmat);
+    return result;
+  };
+
+  const blaze::CompressedMatrix<DataType> filter =
+      wavelet::DaubechiesMat<DataType>(length, 6);
+
+  BENCHMARK("Predefined matrix convolve") {
+    blaze::DynamicVector<DataType> result = filter * signal;
+    return result;
+  };
+}
+
+TEST_CASE("Wavelet algorithms benchmark 2D") {
+  using drift::dsp::NullDenoiseAlgorithm;
+  using drift::dsp::SimpleDenoiseAlgorithm;
+
+  drift::dsp::WaveletParameters parameters = {
+      .signal_shape = {100, 100},
+      .signal_number = 1,
+      .decomposition_steps = 2,
+      .wavelet_type = drift::dsp::WaveletTypes::kDB3};
+
+  auto data_src_100 = GetRandomSignal(100, 100);
+
+  SECTION("Wavelet type") {
+    SECTION("kDB3") {
+      parameters.wavelet_type = drift::dsp::kDB3;
+      WaveletBuffer buffer(parameters);
+
+      BENCHMARK("Decompose 100x100") {
+        buffer.Decompose(data_src_100, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose 100x100") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+
+    SECTION("kDB4") {
+      parameters.wavelet_type = drift::dsp::kDB4;
+      WaveletBuffer buffer(parameters);
+
+      BENCHMARK("Decompose 100x100") {
+        buffer.Decompose(data_src_100, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose 100x100") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+  }
+
+  SECTION("Wavelet step") {
+    SECTION("2 steps") {
+      parameters.decomposition_steps = 2;
+      WaveletBuffer buffer(parameters);
+
+      BENCHMARK("Decompose 100x100") {
+        buffer.Decompose(data_src_100, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose 100x100") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+
+    SECTION("3 steps") {
+      parameters.decomposition_steps = 3;
+      WaveletBuffer buffer(parameters);
+
+      BENCHMARK("Decompose 100x100") {
+        buffer.Decompose(data_src_100, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose 100x100") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+    SECTION("4 steps") {
+      parameters.decomposition_steps = 4;
+      WaveletBuffer buffer(parameters);
+
+      BENCHMARK("Decompose 100x100") {
+        buffer.Decompose(data_src_100, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose 100x100") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+  }
+
+  SECTION("Wavelet decomposition type") {
+    WaveletBuffer buffer(parameters);
+    BENCHMARK("Decompose 100x100") {
+      buffer.Decompose(data_src_100, NullDenoiseAlgorithm<DataType>());
+    };
+
+    BENCHMARK("Compose 100x100") {
+      SignalN2D data_dst;
+      buffer.Compose(&data_dst);
+    };
+  }
+
+  SECTION("Wavelet image size to 60~50 width subbands") {
+    SECTION("800×600") {
+      parameters.signal_shape = {800, 600};
+      parameters.decomposition_steps = 5;
+
+      WaveletBuffer buffer(parameters);
+      auto data_src = GetRandomSignal(600, 800);
+
+      BENCHMARK("Decompose") {
+        buffer.Decompose(data_src, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+
+    SECTION("1920×1080") {
+      parameters.signal_shape = {1920, 1080};
+      parameters.decomposition_steps = 5;
+      WaveletBuffer buffer(parameters);
+      auto data_src = GetRandomSignal(1080, 1920);
+
+      BENCHMARK("Decompose") {
+        buffer.Decompose(data_src, NullDenoiseAlgorithm<DataType>());
+      };
+
+      BENCHMARK("Compose") {
+        SignalN2D data_dst;
+        buffer.Compose(&data_dst);
+      };
+    }
+  }
+
+  SECTION("Serialization") {
+    parameters.signal_shape = {800, 600};
+    parameters.decomposition_steps = 5;
+    WaveletBuffer buffer(parameters);
+    auto data_src = GetRandomSignal(600, 800);
+
+    uint8_t compression_level = GENERATE(0, 8, 16);
+    std::string blob;
+
+    buffer.Decompose(data_src, SimpleDenoiseAlgorithm<DataType>(0.7));
+    REQUIRE(buffer.Serialize(&blob, compression_level));
+
+    BENCHMARK("Parse 800x600 with 5 steps: compression_level " +
+              std::to_string(compression_level)) {
+      auto b = WaveletBuffer::Parse(blob);
+      return b;
+    };
+
+    BENCHMARK("Serialize 800x600 with 5 steps: compression_level = " +
+              std::to_string(compression_level)) {
+      auto ret = buffer.Serialize(&blob, compression_level);
+      return ret;
+    };
+  }
+}
