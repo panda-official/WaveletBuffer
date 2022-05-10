ce28df26fd61606dbd20288b27b521e2be14da84
Kataev Victor <kataev.victor.1234@gmail.com>
2022-05-10 21:01:26 +0000

    Add conan directory

---
+++ conan/.bashrc
@@ -0,0 +1,3 @@
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/development
+source /home/kataev/.local/bin/virtualenvwrapper.sh
---
+++ conan/conanfile.py
@@ -0,0 +1,59 @@
+import os
+
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conans.tools import Git
+
+
+class WaveletBufferConan(ConanFile):
+    name = 'wavelet_buffer'
+    version = '0.1.0'
+    license = 'MPL-2.0'
+    author = 'PANDA GmbH'
+    description = 'An universal C++ compression library based on wavelet ' \
+                  'transformation '
+    url = 'https://github.com/panda-official/WaveletBuffer'
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def set_version(self):
+        build_id = os.getenv("CI_JOB_ID")
+        if build_id and os.getenv("CI_COMMIT_BRANCH"):
+            self.version += f'-b.{build_id}'
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        local_source = os.getenv('CONAN_SOURCE_DIR')
+        if local_source is not None:
+            self.run('cp {} -r {}'.format(
+                os.getenv('CONAN_SOURCE_DIR'), self.source_folder))
+        else:
+            branch = f'v{self.version}' if self.channel == 'stable' else self.channel
+            git = Git()
+            git.clone('https://github.com/panda-official/WaveletBuffer.git',
+                      branch=branch, shallow=True)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["wavelet_toolbox"]
