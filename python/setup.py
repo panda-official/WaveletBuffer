a91d807eacf80de6f6d66bdc65cd53e5a3ba689c
Kataev Victor <kataev.victor.1234@gmail.com>
2022-06-13 14:42:10 +0000

    Update metainfo

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,36 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
-from setuptools import find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='wavelet-buffer',
+    version='0.1.0',
     packages=['wavelet_buffer'],
     package_dir={"": "src"},
-    cmake_install_dir='src/wavelet_buffer'
+    cmake_install_dir='src/wavelet_buffer',
+    author="PANDA, GmbH",
+    author_email="info@panda.technology",
+    description="WaveletBuffer python3 module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/panda-official/WaveletBuffer",
+    python_requires='>=3.6',
+    license='MPL-2.0',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+    ]
 )
