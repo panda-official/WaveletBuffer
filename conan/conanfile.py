dbf24df12d818c0beb599196835dd6c0f9d534f9
Kataev Victor <kataev.victor.1234@gmail.com>
2022-06-29 11:32:10 +0000

    Add conan

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -12,7 +12,7 @@ class WaveletBufferConan(ConanFile):
     author = "PANDA GmbH"
     description = "An universal C++ compression library based on wavelet transformation"
     url = "https://github.com/panda-official/WaveletBuffer"
-    requires = "nlohmann_json/3.10.5", "cereal/1.3.1", "openblas/0.3.17", "blaze/3.8"
+    requires = "openblas/0.3.20", "blaze/3.8"
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
