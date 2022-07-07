f94ec002bc7e27f55e469d01c2439c51c133a1f1
Aleksey Timin <atimin@gmail.com>
2022-07-07 17:20:46 +0200

    copy wavelet buffer to python package

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,7 @@
 import os
+import shutil
 from string import Template
+from setuptools.command.sdist import sdist
 
 try:
     from skbuild import setup  # This line replaces 'from setuptools import setup'
@@ -9,8 +11,8 @@ except ModuleNotFoundError:
 
 from pathlib import Path
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+HERE = Path(__file__).parent
+DESC = (HERE / "README.md").read_text()
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 1
@@ -19,6 +21,42 @@ PATCH_VERSION = 0
 PACKAGE_NAME = "wavelet-buffer"
 
 VERSION_SUFFIX = os.getenv("VERSION_SUFFIX")
+
+
+def get_file_list(path):
+    return path, [f"{folder}/{el}" for folder, _, elements in os.walk(path) for el in elements]
+
+
+class BuildPyCommand(sdist):
+    """Custom build command."""
+
+    def run(self):
+        self.copy_wavelet_buffer_source()
+        sdist.run(self)
+
+    @staticmethod
+    def copy_wavelet_buffer_source():
+        """We copy WaveletBuffer source when we build sdist because it is in the top folder"""
+        project_root = HERE.parent.resolve().parent.resolve()
+        map = [("", [project_root / "CMakeLists.txt"]),
+               ("sources", [project_root / "sources"]),
+               ("wavelet_buffer", [project_root / "wavelet_buffer"]),
+               ("cmake", [project_root / "cmake"])]
+
+        dest = HERE / "ext"
+        if dest.exists():
+            shutil.rmtree(dest)
+
+        Path.mkdir(dest)
+        for folder, files in map:
+            print(folder)
+            new_path = dest / folder
+            for src in files:
+                print(f"copy {src} -> {new_path}")
+                if Path.is_file(src):
+                    shutil.copy(src, new_path)
+                else:
+                    shutil.copytree(src, new_path)
 
 
 def update_package_version(path: Path, version: str, protoc_version: str):
@@ -42,25 +80,31 @@ def build_version():
 
 setup(
     name=PACKAGE_NAME,
+    cmdclass={
+        "sdist": BuildPyCommand
+    },
     version=build_version(),
     packages=["wavelet_buffer"],
-    package_dir={"": "src"},
-    cmake_install_dir="src/wavelet_buffer",
+    package_dir={"": "pkg"},
+    data_files=[get_file_list("src"), get_file_list("ext"),
+                ("", ["CMakeLists.txt"]), ],
+
     extras_require={"test": ["pytest", "numpy"]},
     author="PANDA, GmbH",
     author_email="info@panda.technology",
     description="A universal C++ compression library based on wavelet transformation",
-    long_description=long_description,
+    long_description=DESC,
     long_description_content_type="text/markdown",
     url="https://github.com/panda-official/WaveletBuffer",
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     license="MPL-2.0",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Beta",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: Unix",
+        "Operating System :: MacOS"
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
