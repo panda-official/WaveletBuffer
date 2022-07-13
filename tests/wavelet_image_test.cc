6255483f7d08965aafa1cf0571b23372820bad35
Kataev Victor <kataev.victor.1234@gmail.com>
2022-07-13 08:25:14 +0000

    Add wavelet image

---
+++ tests/wavelet_image_test.cc
@@ -0,0 +1,105 @@
+// Copyright 2020-2022 PANDA GmbH
+
+#include "wavelet_buffer/wavelet_image.h"
+
+#include <fstream>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include "wavelet_buffer/codecs/jpeg_codecs.h"
+#include "wavelet_buffer/wavelet_buffer_view.h"
+
+using drift::Distance;
+using drift::NullDenoiseAlgorithm;
+using drift::SimpleDenoiseAlgorithm;
+using drift::WaveletImage;
+using drift::WaveletParameters;
+using drift::WaveletTypes;
+using drift::codecs::HslJpegCodec;
+
+const WaveletParameters kWaveletParam{
+    .signal_shape = {800, 500},
+    .signal_number = 3,
+    .decomposition_steps = 4,
+    .wavelet_type = WaveletTypes::kDB2,
+};
+
+const auto kImagePath = "fixtures/pandas.jpg";
+const auto kDumpPath = "dump.bin";
+
+TEST_CASE("Wavelet Image") {
+  HslJpegCodec codec;
+  NullDenoiseAlgorithm<float> denoiser;
+
+  SECTION("should import and export JPEG file") {
+    WaveletImage image(kWaveletParam);
+    auto status = image.ImportFromFile(kImagePath, denoiser, codec);
+
+    REQUIRE(WaveletImage::Status::kOk == status);
+
+    status = image.ExportToFile("pandas_out.jpg", codec);
+    REQUIRE(WaveletImage::Status::kOk == status);
+
+    WaveletImage new_images(kWaveletParam);
+
+    status = new_images.ImportFromFile("pandas_out.jpg", denoiser, codec);
+    REQUIRE(WaveletImage::Status::kOk == status);
+    REQUIRE_THAT(new_images.CompareTo(image),
+                 Catch::Matchers::WithinAbs(0, 0.006));
+  }
+
+  SECTION("fails") {
+    WaveletImage image(kWaveletParam);
+
+    SECTION("wrong_channel") {
+      auto status = image.ImportFromFile("pandas_out.jpg", denoiser, codec, 1);
+      REQUIRE(WaveletImage::Status::kDecompositionError == status);
+      status = image.ExportToFile("file.jpeg", codec, 3);
+      REQUIRE(WaveletImage::Status::kCompositionError == status);
+    }
+
+    SECTION("to read file") {
+      auto status = image.ImportFromFile("NO_EXISTING_FILE", denoiser, codec);
+      REQUIRE(WaveletImage::Status::kIOError == status);
+    }
+
+    SECTION("to write file") {
+      auto status = image.ExportToFile("/NO_EXISTING_PATH/file.jpeg", codec);
+      REQUIRE(WaveletImage::Status::kIOError == status);
+    }
+  }
+
+  SECTION("should save and load buffer") {
+    WaveletImage image(kWaveletParam);
+
+    auto status = image.ImportFromFile(kImagePath, denoiser, codec);
+
+    REQUIRE(WaveletImage::Status::kOk == status);
+
+    status = image.Save(kDumpPath);
+    REQUIRE(WaveletImage::Status::kOk == status);
+
+    auto new_image = WaveletImage::Load(kDumpPath);
+    REQUIRE(image.const_buffer() == new_image->const_buffer());
+  }
+
+  SECTION("should store few images as channels in WaveletBuffer") {
+    auto two_img_param = kWaveletParam;
+    two_img_param.signal_number = 6;
+
+    WaveletImage image2(two_img_param);
+
+    REQUIRE(image2.ImportFromFile(kImagePath, denoiser, codec, 3) ==
+            WaveletImage::Status::kOk);
+    REQUIRE(image2.ExportToFile("pandas_out.jpg", codec, 3) ==
+            WaveletImage::Status::kOk);
+
+    WaveletImage image(kWaveletParam);
+    REQUIRE(image.ImportFromFile("pandas_out.jpg", denoiser, codec) ==
+            WaveletImage::Status::kOk);
+
+    REQUIRE_THAT(Distance(image2.const_buffer()(3, 3), image.const_buffer()),
+                 Catch::Matchers::WithinAbs(0, 0.006));
+  }
+}
---
+++ wavelet_buffer/codecs/jpeg_codecs.h
@@ -0,0 +1,83 @@
+// Copyright 2020-2022 PANDA GmbH
+
+#ifndef WAVELET_BUFFER_CODECS_JPEG_CODECS_H_
+#define WAVELET_BUFFER_CODECS_JPEG_CODECS_H_
+
+#include <blaze/Blaze.h>
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <boost/gil/extension/io/jpeg.hpp>
+#include <boost/gil/image.hpp>
+#include <boost/gil/typedefs.hpp>
+
+#include "wavelet_buffer/codecs/color_space.h"
+#include "wavelet_buffer/codecs/image_codec.h"
+
+namespace drift::codecs {
+/**
+ * Class to read and write JPEGs by using RGB color scheme
+ */
+class RgbJpegCodec : public IImageCodec {
+ public:
+  RgbJpegCodec();
+  explicit RgbJpegCodec(float quality);
+
+  [[nodiscard]] bool Decode(const std::string& blob, SignalN2D* image,
+                            size_t start_channel = 0) const override;
+
+  [[nodiscard]] bool Encode(const SignalN2D& image, std::string* blob,
+                            size_t start_channel = 0) const override;
+
+  [[nodiscard]] size_t channel_number() const override { return 3; }
+
+ private:
+  float quality_;
+};
+
+/**
+ * Class to read and write JPEGs by using HSL color scheme
+ */
+class HslJpegCodec : public IImageCodec {
+ public:
+  HslJpegCodec();
+  explicit HslJpegCodec(float quality);
+
+  [[nodiscard]] bool Decode(const std::string& blob, SignalN2D* image,
+                            size_t start_channel = 0) const override;
+
+  [[nodiscard]] bool Encode(const SignalN2D& image, std::string* blob,
+                            size_t start_channel = 0) const override;
+
+  [[nodiscard]] size_t channel_number() const override { return 3; }
+
+ private:
+  float quality_;
+};
+
+/**
+ * Class to read and write JPEGs by using gray color scheme
+ */
+class GrayJpegCodec : public IImageCodec {
+ public:
+  GrayJpegCodec();
+  explicit GrayJpegCodec(float quality);
+
+  [[nodiscard]] bool Decode(const std::string& blob, SignalN2D* image,
+                            size_t start_channel = 0) const override;
+
+  [[nodiscard]] bool Encode(const SignalN2D& image, std::string* blob,
+                            size_t start_channel = 0) const override;
+
+  [[nodiscard]] size_t channel_number() const override { return 1; }
+
+ private:
+  float quality_;
+};
+
+}  // namespace drift::codecs
+
+#endif  // WAVELET_BUFFER_CODECS_JPEG_CODECS_H_
