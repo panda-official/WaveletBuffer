5f4a9b1e7b3f69db72292dd5b29435f6ae4c82d1
Kataev Victor <kataev.victor.1234@gmail.com>
2022-07-17 19:12:33 +0000

    Add codecs test and WI example

--- python/examples/wavelet_buffer_example.py
+++ python/examples/wavelet_buffer_example.py
@@ -40,3 +40,26 @@ buffer = WaveletBuffer(
     decomposition_steps=4,
     wavelet_type=WaveletType.DB1,
 )
+
+with open("../../test/fixtures/pandas.jpg", "rb") as f:
+    blob = f.read()
+
+code = codecs.HslJpeg()
+img = code.decode(blob)
+print(f"Image shape {img.shape}")
+
+buffer.decompose(img, denoiser)
+
+print(f"Channels {len(buffer.decompositions)}")
+print(
+    f"Subbands for channel=0: decomposition_steps*3 +1  {len(buffer.decompositions[0])}"
+)
+print(f"The last subband is approximation:  {buffer.decompositions[0][12]}")
+
+print(f"Reconstructed image x2 smaller and save")
+smaller_img: np.ndarray = buffer.compose(scale_factor=1)
+print(smaller_img.shape)
+blob = code.encode(smaller_img)
+
+with open("2x_pandas.jpg", "wb") as f:
+    f.write(blob)
