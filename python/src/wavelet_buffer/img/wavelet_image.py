6acda9035d6b21f304c0136b2e0d57f17149ac91
Alexey Timin <atimin@gmail.com>
2022-10-24 13:26:55 +0200

    add doc strings

--- python/src/wavelet_buffer/img/wavelet_image.py
+++ python/src/wavelet_buffer/img/wavelet_image.py
@@ -33,6 +33,13 @@ class WaveletImage(impl.WaveletImage):
         codec: BaseCodec,
         start_channel: int = 0,
     ) -> None:
+        """Import an image from file and decompose it with WaveletBuffer
+        Args:
+            file_path: path to image
+            denoiser: denoising algorithm
+            codec: a coodec for decoding the image format
+            start_channel: need if you want to decode specific channel as a monochrome image
+        """
         super(WaveletImage, self).import_from_file(
             file_path, denoiser, codec._impl, start_channel
         )
@@ -40,6 +47,13 @@ class WaveletImage(impl.WaveletImage):
     def export_to_file(
         self, file_path: str, codec: BaseCodec, start_channel: int = 0
     ) -> None:
+        """Encoding a wavelet image to a file with common image format e.g. JPEG
+        Args:
+            file_path: path to image
+            codec:  a coodec for encoding the image format
+            start_channel: need if you want to encode specific channel as a monochrome image, or you have
+             a few images in the buffer
+        """
         super(WaveletImage, self).export_to_file(file_path, codec._impl, start_channel)
 
     def import_from_string(
@@ -49,23 +63,50 @@ class WaveletImage(impl.WaveletImage):
         codec: BaseCodec,
         start_channel: int = 0,
     ) -> None:
+        """Import an image from byte sting and decompose it with WaveletBuffer
+        Args:
+            data: encoded image e.g. JPEG string
+            denoiser: denoising algorithm
+            codec: a coodec for decoding the image format
+            start_channel: need if you want to decode specific channel as a monochrome image
+        """
         super(WaveletImage, self).import_from_string(
             data, denoiser, codec._impl, start_channel
         )
 
     def export_to_string(self, codec: BaseCodec, start_channel: int = 0) -> bytes:
+        """Encoding a wavelet image to a byte sting with common image format e.g. JPEG
+        Args:
+            codec:  a coodec for encoding the image format
+            start_channel: need if you want to encode specific channel as a monochrome image, or you have
+             a few images in the buffer
+        Returns:
+            encoded image e.g. JPEG string
+        """
         return super(WaveletImage, self).export_to_string(codec._impl, start_channel)
 
     @staticmethod
     def load(file_path: str) -> "WaveletImage":
+        """Deserialize a wavelet image from file
+        Args:
+            file_path:
+        Returns:
+            decompressed image
+        """
         return impl.WaveletImage.load(file_path)
 
     def save(self, file_path: str) -> None:
+        """Serialize image to a file
+        Args:
+            file_path:
+        """
         super(WaveletImage, self).save(file_path)
 
     def distance(self, other: "WaveletImage") -> float:
+        """Distance to other image. 1 - the same, 0 - something completely different"""
         return super(WaveletImage, self).distance(other)
 
     @property
     def buffer(self) -> WaveletBuffer:
+        """Get wavelet buffer"""
         return super(WaveletImage, self).buffer
