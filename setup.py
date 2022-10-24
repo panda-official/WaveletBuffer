edf2463f5a8fc63beddc9b3dcb9b65e4b37a3ece
Alexey Timin <atimin@gmail.com>
2022-10-24 16:37:25 +0200

    use setup.py for extra deps

--- setup.py
+++ setup.py
@@ -55,6 +55,17 @@ setup(
     url="https://github.com/panda-official/WaveletBuffer",
     python_requires=">=3.8",
     license="MPL-2.0",
+    extra_require={
+        "docs": [
+            "mkdocs~=1.3",
+            "git+https://github.com/JakubAndrysek/mkdoxy",
+            "mkdocs-material~=8.4",
+            "plantuml-markdown~=3.5",
+            "mkdocs-same-dir~=0.1",
+            "mkdocs-jupyter~=0.21",
+            " mkdocstrings[python]~=0.19",
+        ]
+    },
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
