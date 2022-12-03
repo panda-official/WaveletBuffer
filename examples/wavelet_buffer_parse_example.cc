54385692cff43ce817ff456e885341b14b8eabc3
Kataev Victor <kataev.victor.1234@gmail.com>
2022-12-03 19:02:30 +0000

    Fix

--- examples/wavelet_buffer_parse_example.cc
+++ examples/wavelet_buffer_parse_example.cc
@@ -3,14 +3,17 @@
 #include <wavelet_buffer/wavelet_buffer.h>
 
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 int main(int argc, char* argv[]) {
   /* Read file to std::string */
+  std::cout << argv[1] << std::endl;
   std::ifstream file(argv[1]);
   std::stringstream blob;
   blob << file.rdbuf();
 
+  /* Load wavelet buffer from  binary */
   auto wb = drift::WaveletBuffer::Parse(blob.str());
 
   return 0;
