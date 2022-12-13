60a236e26f4ad7ef7746e71e287a28c4b31d8438
Kataev Victor <kataev.victor.1234@gmail.com>
2022-12-13 13:17:40 +0000

    Fix parse example

--- examples/wavelet_buffer_parse_example.cc
+++ examples/wavelet_buffer_parse_example.cc
@@ -10,37 +10,31 @@
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
+    return EXIT_FAILURE;
   }
 
   std::cout << "Load " << argv[1] << std::endl;
 
-  drift::WaveletParameters params1, params2;
-  blaze::Archive<std::ifstream> archive(argv[1]);
-  archive >> params1 >> params2;
-
-  std::cout << params1 << std::endl;
-  std::cout << params2 << std::endl;
-
-  return 0;
-
-  try {
-    /* Read file to std::string */
-    std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-      std::cerr << "Could not open file " << argv[1] << std::endl;
-      return EXIT_FAILURE;
-    }
-
-    std::stringstream blob;
-    blob << file.rdbuf();
-
-    /* Load wavelet buffer from binary */
-    auto wb = drift::WaveletBuffer::Parse(blob.str());
-  } catch (std::exception& e) {
-    std::cerr << "Failed parse data: " << e.what() << std::endl;
+  /* Read file to std::string */
+  std::ifstream file(argv[1]);
+  if (!file.is_open()) {
+    std::cerr << "Could not open file " << argv[1] << std::endl;
     return EXIT_FAILURE;
   }
-  std::cout << "Loaded" << std::endl;
+
+  std::stringstream blob;
+  blob << file.rdbuf();
+
+  /* Load wavelet buffer from binary */
+  auto wb = drift::WaveletBuffer::Parse(blob.str());
+  if (!wb) {
+    std::cerr << "Could not parse wavelet buffer from file " << argv[1]
+              << std::endl;
+    return EXIT_FAILURE;
+  } else {
+    std::cout << "Loaded wavelet buffer with parameters:" << wb->parameters()
+              << std::endl;
+  }
 
   return EXIT_SUCCESS;
 }
