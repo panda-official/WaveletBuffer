188e364f18de1a369b27e42e13f294219299e66b
Alexey Timin <atimin@gmail.com>
2023-06-23 15:00:45 +0200

    remove sf_compressor artifact

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -84,7 +84,6 @@ class WaveletBufferConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = [
             "wavelet_buffer",
-            "sf_compressor",
             "matrix_compressor",
             "fpzip",
             "streamvbyte",
